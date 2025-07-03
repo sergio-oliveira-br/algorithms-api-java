e53da0f39abccdbf76a2730d598116a265a6bfb7
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2025-07-03 12:05:29 +0100

    Initial commit: Spring Boot Algorithms Backend

---
+++ src/main/java/com/github/sergiooliveirabr/algorithmsapi/AlgorithmsApiApplication.java
@@ -0,0 +1,13 @@
+package com.github.sergiooliveirabr.algorithmsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgorithmsApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlgorithmsApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/sergiooliveirabr/algorithmsapi/AlgorithmsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.sergiooliveirabr.algorithmsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgorithmsApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
