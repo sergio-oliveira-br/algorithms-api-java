2d9ac6da82c3f2218a77bfd92d1da5ff8c38161e
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2025-07-04 12:14:23 +0100

    Create SortController.java

---
+++ src/main/java/com/github/sergiooliveirabr/algorithmsapi/controller/SortController.java
@@ -0,0 +1,23 @@
+package com.github.sergiooliveirabr.algorithmsapi.controller;
+
+import com.github.sergiooliveirabr.algorithmsapi.services.GeneratorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/sort")
+@CrossOrigin(origins = "http://localhost:5173")
+public class SortController {
+
+    private final GeneratorService generatorService;
+
+    @Autowired
+    public SortController(GeneratorService generatorService) {
+        this.generatorService = generatorService;
+    }
+
+    @PostMapping("/generator")
+    public int[] randomizer(@RequestParam int qtyOfNumbersToGenerate) {
+        return generatorService.generateRandomNum(qtyOfNumbersToGenerate);
+    }
+}
