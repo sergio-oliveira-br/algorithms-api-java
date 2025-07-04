76706c1cf6a7ec0f43fa0b7896bf700a9eb0cfc0
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2025-07-04 12:14:21 +0100

    Create GeneratorService.java

---
+++ src/main/java/com/github/sergiooliveirabr/algorithmsapi/services/GeneratorService.java
@@ -0,0 +1,20 @@
+package com.github.sergiooliveirabr.algorithmsapi.services;
+
+import org.springframework.stereotype.Service;
+
+import java.util.Random;
+
+@Service
+public class GeneratorService {
+
+    public int[] generateRandomNum(int qtyOfNumbersToGenerate) {
+
+        Random randomNumGenerator = new Random();
+        int[] generatedNumbers = new int[qtyOfNumbersToGenerate];
+
+        for (int i = 0; i < qtyOfNumbersToGenerate; i++) {
+            generatedNumbers[i] = randomNumGenerator.nextInt(100);
+        }
+        return generatedNumbers;
+    }
+}
