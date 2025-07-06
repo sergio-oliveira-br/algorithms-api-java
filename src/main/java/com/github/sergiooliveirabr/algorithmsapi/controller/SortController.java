5ccb47618d3211292a356a411a3ac7cb264fa60c
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2025-07-06 11:17:11 +0100

    Update SortController.java
    act:
    -> endpoint created to bubble sorte

--- src/main/java/com/github/sergiooliveirabr/algorithmsapi/controller/SortController.java
+++ src/main/java/com/github/sergiooliveirabr/algorithmsapi/controller/SortController.java
@@ -1,6 +1,7 @@
 package com.github.sergiooliveirabr.algorithmsapi.controller;
 
 import com.github.sergiooliveirabr.algorithmsapi.services.randomizer.GeneratorService;
+import com.github.sergiooliveirabr.algorithmsapi.services.sort.BubbleSortService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,14 +11,24 @@ import org.springframework.web.bind.annotation.*;
 public class SortController {
 
     private final GeneratorService generatorService;
+    private final BubbleSortService bubbleSortService;
 
     @Autowired
-    public SortController(GeneratorService generatorService) {
+    public SortController(GeneratorService generatorService,
+                          BubbleSortService bubbleSortService) {
+
         this.generatorService = generatorService;
+        this.bubbleSortService = bubbleSortService;
     }
 
     @PostMapping("/generator")
     public int[] randomizer(@RequestParam int qtyOfNumbersToGenerate) {
         return generatorService.generateRandomNum(qtyOfNumbersToGenerate);
+    }
+
+    @PostMapping("/algorithms")
+    public int[] sortAlgorithms(@RequestParam int[] generatedNumbersArray) {
+        System.out.println("controlador");
+        return bubbleSortService.sortAlgorithm(generatedNumbersArray);
     }
 }
