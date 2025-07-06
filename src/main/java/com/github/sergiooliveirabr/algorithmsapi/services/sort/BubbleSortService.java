546be0687e338d6e1cf2dbc25dea8b93fd835aef
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2025-07-06 11:16:57 +0100

    Create BubbleSortService.java
    act:
    ->bubble sort  method implemented

---
+++ src/main/java/com/github/sergiooliveirabr/algorithmsapi/services/sort/BubbleSortService.java
@@ -0,0 +1,29 @@
+package com.github.sergiooliveirabr.algorithmsapi.services.sort;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class BubbleSortService implements SortAlgorithmsStrategy{
+
+    @Override
+    public int[] sortAlgorithm(int[] arrayToSort) {
+
+        System.out.println("service");
+
+        int arraySize = arrayToSort.length;
+
+        if (arraySize == 0) {
+            throw new IllegalArgumentException("Array is empty");
+        }
+        for (int i = 0; i < arraySize; i++) {
+            for (int j = i + 1; j < arraySize; j++) {
+                if (arrayToSort[i] > arrayToSort[j]) {
+                    int temp = arrayToSort[i];
+                    arrayToSort[i] = arrayToSort[j];
+                    arrayToSort[j] = temp;
+                }
+            }
+        }
+        return arrayToSort;
+    }
+}
