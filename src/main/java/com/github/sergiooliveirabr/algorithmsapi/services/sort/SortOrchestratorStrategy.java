6c726b4b7dc9622808f61c228fa3fcdba882e8c9
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2025-07-09 14:59:41 +0100

    Create SortOrchestratorStrategy.java
    act:
    -> method responsible to orchestrate the strategies via map created

---
+++ src/main/java/com/github/sergiooliveirabr/algorithmsapi/services/sort/SortOrchestratorStrategy.java
@@ -0,0 +1,27 @@
+package com.github.sergiooliveirabr.algorithmsapi.services.sort;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+
+@Service
+public class SortOrchestratorStrategy {
+
+    private final Map<String, SortAlgorithmsStrategy> sortAlgorithmsStrategyMap;
+
+    @Autowired
+    public SortOrchestratorStrategy(Map<String, SortAlgorithmsStrategy> sortAlgorithmsStrategyMap) {
+        this.sortAlgorithmsStrategyMap = sortAlgorithmsStrategyMap;
+    }
+
+    public void sortOrchestrator(int[] arrayToSort, String selectedStrategy) {
+
+        SortAlgorithmsStrategy sortAlgorithmsStrategy = sortAlgorithmsStrategyMap.get(selectedStrategy);
+
+        if (sortAlgorithmsStrategy == null) {
+            throw new RuntimeException("No such strategy: " + selectedStrategy);
+        }
+        sortAlgorithmsStrategy.sortAlgorithm(arrayToSort);
+    }
+}
