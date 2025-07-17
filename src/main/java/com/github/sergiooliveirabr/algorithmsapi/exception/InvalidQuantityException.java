3e78ad4e8079c437e230e18790d7fadcb06f9592
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2025-07-17 19:00:13 +0100

    Create InvalidQuantityException.java
    act:
    -> this exception class is focused on preventing the user from entering with invalid numbers

---
+++ src/main/java/com/github/sergiooliveirabr/algorithmsapi/exception/InvalidQuantityException.java
@@ -0,0 +1,8 @@
+package com.github.sergiooliveirabr.algorithmsapi.exception;
+
+public class InvalidQuantityException extends RuntimeException {
+
+    public InvalidQuantityException(String message) {
+        super(message);
+    }
+}
