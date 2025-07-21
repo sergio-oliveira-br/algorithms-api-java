c60d87c60f6a6ca886c43aa70cd574365a55482d
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2025-07-21 14:31:02 +0100

    Update SortOrchestratorStrategy.java
    act:
    -> method has been included the perfomance timer

--- src/main/java/com/github/sergiooliveirabr/algorithmsapi/services/sort/SortOrchestratorStrategy.java
+++ src/main/java/com/github/sergiooliveirabr/algorithmsapi/services/sort/SortOrchestratorStrategy.java
@@ -1,7 +1,9 @@
 package com.github.sergiooliveirabr.algorithmsapi.services.sort;
 
+import com.github.sergiooliveirabr.algorithmsapi.dto.SortResult;
 import com.github.sergiooliveirabr.algorithmsapi.exception.StrategyNotFoundException;
 import com.github.sergiooliveirabr.algorithmsapi.services.sort.strategy.SortAlgorithmsStrategy;
+import com.github.sergiooliveirabr.algorithmsapi.services.utilities.PerformanceTimer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,19 +13,37 @@ import java.util.Map;
 public class SortOrchestratorStrategy {
 
     private final Map<String, SortAlgorithmsStrategy> sortAlgorithmsStrategyMap;
+    private final PerformanceTimer performanceTimer;
 
     @Autowired
-    public SortOrchestratorStrategy(Map<String, SortAlgorithmsStrategy> sortAlgorithmsStrategyMap) {
+    public SortOrchestratorStrategy(Map<String, SortAlgorithmsStrategy> sortAlgorithmsStrategyMap,
+                                    PerformanceTimer performanceTimer) {
+
         this.sortAlgorithmsStrategyMap = sortAlgorithmsStrategyMap;
+        this.performanceTimer = performanceTimer;
     }
 
-    public int[] sortOrchestrator(int[] arrayToSort, String selectedStrategy) {
+    public SortResult sortOrchestrator(int[] arrayToSort,
+                                       String selectedStrategy) {
 
         SortAlgorithmsStrategy sortAlgorithmsStrategy = sortAlgorithmsStrategyMap.get(selectedStrategy);
 
         if (sortAlgorithmsStrategy == null) {
             throw new StrategyNotFoundException("Strategy: '" + selectedStrategy + "' does not exist.");
         }
-        return sortAlgorithmsStrategy.sortAlgorithm(arrayToSort);
+
+        try {
+            PerformanceTimer.TimedResult<int[]> timedResult = performanceTimer
+                    .elapsedTime(() -> sortAlgorithmsStrategy.sortAlgorithm(arrayToSort));
+
+            // Create and return the SortResult object with the required data
+            return new SortResult(
+                    timedResult.result(),
+                    timedResult.elapsedTimeNanoSeconds(),
+                    selectedStrategy );        
+        }
+        catch (Exception e) {
+            throw new RuntimeException("Failed to sort array using " + selectedStrategy, e);
+        }
     }
 }
