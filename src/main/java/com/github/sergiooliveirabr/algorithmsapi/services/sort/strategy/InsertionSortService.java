3e547df5c54beb847fe108a5e46b471b51b52614
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2025-07-24 13:34:27 +0100

    Create InsertionSortService.java
    act:
    -> Insertion Sort implemented

---
+++ src/main/java/com/github/sergiooliveirabr/algorithmsapi/services/sort/strategy/InsertionSortService.java
@@ -0,0 +1,27 @@
+package com.github.sergiooliveirabr.algorithmsapi.services.sort.strategy;
+
+import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+
+@Service
+public class InsertionSortService implements SortAlgorithmsStrategy {
+
+    @Override
+    public int[] sortAlgorithm(int[] arrayToSort) {
+
+        int arraySize = arrayToSort.length;
+
+        for(int i = 1; i < arraySize; i++) {
+            int currentValue = arrayToSort[i];
+            int possition = i - 1;
+
+            while(possition >= 0 && arrayToSort[possition] > currentValue) {
+                arrayToSort[possition + 1] = arrayToSort[possition];
+                possition--;
+            }
+            arrayToSort[possition + 1] = currentValue;
+        }
+        return arrayToSort;
+    }
+}
