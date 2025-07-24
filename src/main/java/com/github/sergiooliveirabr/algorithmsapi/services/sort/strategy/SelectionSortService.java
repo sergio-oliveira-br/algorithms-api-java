94cd0f41e3dea2ddd0f1e04ae4d19ba251a6241a
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2025-07-24 15:21:55 +0100

    Create SelectionSortService.java
    act:
    -> Selection Sort algorithm implemented

---
+++ src/main/java/com/github/sergiooliveirabr/algorithmsapi/services/sort/strategy/SelectionSortService.java
@@ -0,0 +1,27 @@
+package com.github.sergiooliveirabr.algorithmsapi.services.sort.strategy;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class SelectionSortService implements SortAlgorithmsStrategy {
+
+    @Override
+    public int[] sortAlgorithm(int[] arrayToSort) {
+
+        int arraySize = arrayToSort.length;
+
+        for (int i = 0; i < arraySize; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < arraySize; j++) {
+                if (arrayToSort[j] < arrayToSort[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            int temp = arrayToSort[i];
+
+            arrayToSort[i] = arrayToSort[minIndex];
+            arrayToSort[minIndex] = temp;
+        }
+        return arrayToSort;
+    }
+}
