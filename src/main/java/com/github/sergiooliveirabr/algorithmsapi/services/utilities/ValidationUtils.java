66913bb0d5e5b61f30fa767dad5ff615802331fd
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2025-07-31 11:46:42 +0100

    Update ValidationUtils.java
    act:
    -> exception changed. this is the appropriate exception

--- src/main/java/com/github/sergiooliveirabr/algorithmsapi/services/utilities/ValidationUtils.java
+++ src/main/java/com/github/sergiooliveirabr/algorithmsapi/services/utilities/ValidationUtils.java
@@ -1,11 +1,13 @@
 package com.github.sergiooliveirabr.algorithmsapi.services.utilities;
 
+import com.github.sergiooliveirabr.algorithmsapi.exception.InvalidQuantityException;
+
 public class ValidationUtils {
 
     public static void validateArray (int[] array) {
-        
+
         if (array == null || array.length == 0) {
-            throw new IllegalArgumentException("Array cannot be null or empty");
+            throw new InvalidQuantityException("Array cannot be null or empty");
         }
     }
 }
