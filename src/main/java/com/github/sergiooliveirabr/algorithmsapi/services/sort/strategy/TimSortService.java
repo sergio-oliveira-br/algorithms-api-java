68e3511cda8868836901c27483149f1eea735094
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2025-07-31 11:53:23 +0100

    Update TimSortService.java
    act:
    -> validation implemented

--- src/main/java/com/github/sergiooliveirabr/algorithmsapi/services/sort/strategy/TimSortService.java
+++ src/main/java/com/github/sergiooliveirabr/algorithmsapi/services/sort/strategy/TimSortService.java
@@ -1,5 +1,6 @@
 package com.github.sergiooliveirabr.algorithmsapi.services.sort.strategy;
 
+import com.github.sergiooliveirabr.algorithmsapi.services.utilities.ValidationUtils;
 import org.springframework.stereotype.Service;
 
 import java.util.Arrays;
@@ -9,7 +10,10 @@ public class TimSortService implements SortAlgorithmsStrategy {
 
     @Override
     public int[] sortAlgorithm(int[] arrayToSort) {
+
         int[] sortedArray = Arrays.copyOf(arrayToSort, arrayToSort.length);
+
+        ValidationUtils.validateArray(arrayToSort);
         Arrays.sort(sortedArray);
 
         return sortedArray;
