766da2ddbee6a9d010c219eb6353fcad083b2aaf
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2025-08-03 11:33:45 +0100

    Update FindMinOrMaxController.java
    act:
    -> new endpoint added to implemente the strategy pattern

--- src/main/java/com/github/sergiooliveirabr/algorithmsapi/controller/FindMinOrMaxController.java
+++ src/main/java/com/github/sergiooliveirabr/algorithmsapi/controller/FindMinOrMaxController.java
@@ -1,10 +1,14 @@
 package com.github.sergiooliveirabr.algorithmsapi.controller;
 
 import com.github.sergiooliveirabr.algorithmsapi.services.linearsearch.FindMaxService;
+import com.github.sergiooliveirabr.algorithmsapi.services.linearsearch.FindMinOrMaxInterface;
+import com.github.sergiooliveirabr.algorithmsapi.services.linearsearch.FindMinOrMaxOrchestrator;
 import com.github.sergiooliveirabr.algorithmsapi.services.linearsearch.FindMinService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/find")
@@ -13,12 +17,15 @@ public class FindMinOrMaxController {
 
     private final FindMinService findMinService;
     private final FindMaxService findMaxService;
+    private final FindMinOrMaxOrchestrator findMinOrMaxOrchestrator;
 
     @Autowired
     public FindMinOrMaxController(FindMinService findMinService,
-                                  FindMaxService findMaxService) {
+                                  FindMaxService findMaxService,
+                                  FindMinOrMaxOrchestrator findMinOrMaxOrchestrator) {
         this.findMinService = findMinService;
         this.findMaxService = findMaxService;
+        this.findMinOrMaxOrchestrator = findMinOrMaxOrchestrator;
     }
 
     @PostMapping("/min-value")
@@ -31,5 +38,13 @@ public class FindMinOrMaxController {
     public ResponseEntity<Integer> findMax(@RequestBody int[] array) {
         int maxValue = findMaxService.findMinOrMax(array);
         return ResponseEntity.ok(maxValue);
+    }
+
+    @PostMapping("/min-or-max")
+    public ResponseEntity<Integer> findMinOrMax(@RequestBody int[] array,
+                                                @RequestBody String findMinOrMaxStrategy) {
+
+        int resultFound = findMinOrMaxOrchestrator.executeStrategy(findMinOrMaxStrategy, array);
+        return ResponseEntity.ok(resultFound);
     }
 }
