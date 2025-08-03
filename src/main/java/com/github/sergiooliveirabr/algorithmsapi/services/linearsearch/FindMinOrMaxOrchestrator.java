80f7e9d3ec2dcf2870bcf247a2b8b736069cd623
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2025-08-03 11:33:13 +0100

    Create FindMinOrMaxOrchestrator.java
    act:
    -> Orchestrator implemented

---
+++ src/main/java/com/github/sergiooliveirabr/algorithmsapi/services/linearsearch/FindMinOrMaxOrchestrator.java
@@ -0,0 +1,28 @@
+package com.github.sergiooliveirabr.algorithmsapi.services.linearsearch;
+
+import com.github.sergiooliveirabr.algorithmsapi.exception.StrategyNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+
+@Service
+public class FindMinOrMaxOrchestrator {
+
+    private final Map<String, FindMinOrMaxInterface> findMinMaxOrchestratorMap;
+
+    public FindMinOrMaxOrchestrator(Map<String, FindMinOrMaxInterface> findMinMaxOrchestrator) {
+        this.findMinMaxOrchestratorMap = findMinMaxOrchestrator;
+    }
+
+    public int executeStrategy(String StrategyName, int[] array) {
+
+        FindMinOrMaxInterface strategy = findMinMaxOrchestratorMap.get(StrategyName);
+
+        if (strategy == null) {
+            throw new StrategyNotFoundException("Strategy " + StrategyName + " not found");
+
+        }
+        return strategy.findMinOrMax(array);
+
+    }
+}
