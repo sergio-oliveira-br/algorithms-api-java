b912d6b11f687ddb2131164f6d67246d6c705c6d
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2025-08-21 18:31:12 +0100

    Update CheckDuplicatesService.java
    act:
    -> the service class now returns a complete object with all information by using the DTO created

--- src/main/java/com/github/sergiooliveirabr/algorithmsapi/services/checkduplicates/CheckDuplicatesService.java
+++ src/main/java/com/github/sergiooliveirabr/algorithmsapi/services/checkduplicates/CheckDuplicatesService.java
@@ -1,26 +1,35 @@
 package com.github.sergiooliveirabr.algorithmsapi.services.checkduplicates;
 
+import com.github.sergiooliveirabr.algorithmsapi.dto.CheckResult;
 import com.github.sergiooliveirabr.algorithmsapi.services.utilities.ValidationUtils;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Set;
 
 @Service
 public class CheckDuplicatesService {
 
-    public boolean isDuplicate(int[] array) {
+    public CheckResult checkDuplicates (int[] array) {
 
         ValidationUtils.validateArray(array);
 
         Set<Integer> seenNumbers = new HashSet<>();
+        Set<Integer> duplicates = new HashSet<>();
 
         for(int number: array) {
             if (seenNumbers.contains(number)){
-                return true;
+               duplicates.add(number);
             }
             seenNumbers.add(number);
         }
-        return false;
+
+        CheckResult result = new CheckResult();
+        result.setHasDuplicate(!duplicates.isEmpty());
+        result.setDuplicateNumbers(new ArrayList<>(duplicates));
+        result.setDuplicateCount(duplicates.size());
+
+        return result;
     }
 }
